1cd9b5147eec2ed4313537e7d4ed14861cb7cd5e
Daianes26 <daianeasilva26@gmail.com>
2019-11-24 21:52:42 -0500

    AlertComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { AppRoutingModule } from './app-routing.model';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor-service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -35,8 +37,10 @@ import { AuthInterceptorService } from './auth/auth-interceptor-service';
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
-   
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
+
   ],
   imports: [
     BrowserModule,
@@ -55,6 +59,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor-service';
        multi: true
       }
     ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], 
+  entryComponents: [
+    AlertComponent
+  ]
 })
 export class AppModule { }
