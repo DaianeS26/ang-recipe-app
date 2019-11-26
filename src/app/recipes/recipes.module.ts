1bde261d1c4e0fbb31b94d0295b4bb3efaeacacb
Daianes26 <daianeasilva26@gmail.com>
2019-11-25 21:00:02 -0500

    Core Modules and shared modules

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -7,9 +7,9 @@ import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.compo
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -22,17 +22,9 @@ import { RecipesRoutingModule } from './recipes-routing.module';
     ],
     imports: [
         RouterModule,
-        CommonModule,
         ReactiveFormsModule,
+        SharedModule,
         RecipesRoutingModule
-    ],
-    exports: [
-        RecipesComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeItemComponent,
-        RecipeStartComponent,
-        RecipeEditComponent
     ]
 })
 export class RecipesModule {}
