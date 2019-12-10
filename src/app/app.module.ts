d7719900004282a71092430403589efbbad339cb
Daianes26 <daianeasilva26@gmail.com>
2019-12-09 19:11:34 -0500

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,21 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AppRoutingModule } from './app-routing.model';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
+
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
+import { AppRoutingModule } from './app-routing.model';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule, 
-    ShoppingListModule,
+    AppRoutingModule,
     SharedModule,
-    CoreModule, 
-    AuthModule
-
+    CoreModule
   ],
-
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
