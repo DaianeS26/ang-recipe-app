d9faebb183eef0aad89f57a682fe9148947716a4
Daianes26 <daianeasilva26@gmail.com>
2019-12-13 22:03:22 -0500

    Remove shopping list services and update

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,6 @@
 import {  Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import {Ingredient} from '../shared/ingredient.model';
-import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Subject } from 'rxjs';
 import { Store } from '@ngrx/store';
 import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
@@ -38,7 +37,6 @@ export class RecipeService {
     private recipes: Recipe[] = [];
 
     constructor(
-        private slService: ShoppingListService,
         private store: Store<fromShoppingList.AppState>
     ){}
 
