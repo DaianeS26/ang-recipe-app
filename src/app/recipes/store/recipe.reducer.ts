b56984d7fd0c632d7a590c18efa720890671b831
Daianes26 <daianeasilva26@gmail.com>
2019-12-19 22:14:43 -0500

    Add, update, delete recipes ngrx/effects

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -9,13 +9,39 @@ const initialState: State = {
     recipes: []
 };
 
-
 export function recipeReducer(state = initialState, action: RecipeActions.RecipeActions){
     switch(action.type){
         case RecipeActions.SET_RECIPES:
             return {
                ...state,
                recipes: [...action.payload] 
+            };
+        case RecipeActions.ADD_RECIPE:
+            return {
+                ...state,
+                recipes: [...state.recipes, action.payload]
+            };
+        case RecipeActions.UPDATE_RECIPE:
+            const updatedRecipe = {
+                ...state.recipes[action.payload.index], 
+                ...action.payload.newRecipe
+            };
+
+            const updatedRecipes = [... state.recipes];
+            updatedRecipes[action.payload.index] = updatedRecipe;
+
+            return {
+                ...state,
+                recipes: updatedRecipes
+
+            };
+        case RecipeActions.DELETE_RECIPE:
+            return {
+                ...state, 
+                recipes: state.recipes.filter((recipe, index) => {
+                    //if the index of the action payload doesn't match. It is not the element we want to remove
+                    return index !== action.payload;
+                })
             };
         default:
             return state;
